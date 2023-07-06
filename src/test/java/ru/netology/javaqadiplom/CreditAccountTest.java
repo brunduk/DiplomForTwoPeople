ba76b0650f309fbe1e1bf97cdcacee3b28c60f03
double d <brunduk4415@gmail.com>
2023-07-06 18:14:50 +0300

    work done

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -17,4 +17,133 @@ public class CreditAccountTest {
 
         Assertions.assertEquals(3_000, account.getBalance());
     }
+
+    @Test
+    public void negativeScenario() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                -15
+        );
+
+        Assertions.assertThrows(IllegalArgumentException.class, () ->{
+            account.add(3000);
+        });
+    }
+
+    @Test
+    public void payShouldBalance() {
+        CreditAccount account = new CreditAccount(
+                5_000,
+                5_000,
+                10
+        );
+
+        account.pay(2_000);
+
+        Assertions.assertEquals(3_000, account.getBalance());
+    }
+
+    @Test
+    public void payMoreCreditLimit() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        Assertions.assertEquals(false, account.pay(10_000));
+    }
+
+    @Test
+    public void payOnCreditLimit() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        Assertions.assertEquals(true, account.pay(1_000));
+    }
+
+    @Test
+    public void payNegativeAmount() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        Assertions.assertEquals(false, account.pay(-1_000));
+    }
+
+    @Test
+    public void addSumOnCard() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        Assertions.assertEquals(true, account.add(1_000));
+    }
+
+    @Test
+    public void addNegativeSumOnCard() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        Assertions.assertEquals(false, account.add(-1_000));
+    }
+
+    @Test
+    public void addSumOnZeroBalanceShould() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        account.add(1_000);
+
+        Assertions.assertEquals(1_000, account.getBalance());
+    }
+
+    @Test
+    public void addSumOnBalanceShould() {
+        CreditAccount account = new CreditAccount(
+                1_000,
+                5_000,
+                15
+        );
+
+        account.add(1_000);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+
+    @Test
+    public void yearPercentNegativeBalance() {
+        CreditAccount account = new CreditAccount(
+                -1000,
+                5_000,
+                10
+        );
+
+        Assertions.assertEquals(-100, account.yearChange());
+    }
+
+    @Test
+    public void yearPercentPositiveBalance() {
+        CreditAccount account = new CreditAccount(
+                1000,
+                5_000,
+                10
+        );
+
+        Assertions.assertEquals(0, account.yearChange());
+    }
 }
