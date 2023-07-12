adca8992fdace5f884e42fe9f88851b89c67820f
double d <brunduk4415@gmail.com>
2023-07-12 22:17:31 +0300

    green check mark done

--- src/main/java/ru/netology/javaqadiplom/IllegalArgumentException.java
+++ src/main/java/ru/netology/javaqadiplom/IllegalArgumentException.java
@@ -3,4 +3,8 @@ public class IllegalArgumentException extends RuntimeException{
     public IllegalArgumentException(int rate) {
         super("Накопительная ставка не может быть отрицательной, а у вас: " + rate);
     }
+
+    public IllegalArgumentException(String s) {
+        super(s);
+    }
 }
--- src/main/java/ru/netology/javaqadiplom/NegativeRateException.java
+++ src/main/java/ru/netology/javaqadiplom/NegativeRateException.java
@@ -3,4 +3,8 @@ public class NegativeRateException extends IllegalArgumentException {
     public NegativeRateException(int rate) {
         super("Накопительная ставка не может быть отрицательной, а у вас: " + rate);
     }
+
+    public NegativeRateException (String s) {
+        super(s);
+    }
 }
